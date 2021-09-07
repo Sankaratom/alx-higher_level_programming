8252181d1086905bdb464dfca23d833a7cfd4946
Sankaratom <bekalutilahun39@gmail.com>
2021-09-07 22:29:11 +0300

    using replace

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
-        if (i != len(str) - 1):
-            if (96 < ord(str[i]) < 123):
-                print(chr(ord(str[i]) - 32), end='')
-            else:
-                print(str[i], end='')
-        else:
-            if (96 < ord(str[i]) < 123):
-                print(chr(ord(str[i]) - 32))
-            else:
-                print(str[i])
+        if (96 < ord(str[i]) < 123):
+            str = str.replace(str[i], chr(ord(str[i]) - 32))
+    print(str)
