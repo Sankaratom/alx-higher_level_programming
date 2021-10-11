b24882afb2bf9b28869349280079da993776ade1
Sankaratom <bekalutilahun39@gmail.com>
2021-10-11 16:44:48 +0300

    class creation fi using pass to duit widoout defining anything

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/bin/bash/python3
-''' A module for creating a dummy class with the name `Square '''
+"""A module for creating a dummy class with the name `Square """
 
 
 class Square:
-    ''' Using pass to not define the Square class '''
+    """Using pass to not define the Square class"""
     pass
