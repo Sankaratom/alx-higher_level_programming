88b95eb76f6fb8a19a8e389389ed9b78dcab807e
Sankaratom <bekalutilahun39@gmail.com>
2021-10-20 18:27:32 +0300

    Value and type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,16 +6,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError:
-        print("Type Error")
-        return False
     except ValueError:
         print("Value Error")
         return False
-    except NameError:
-        print("Name Error")
-        return False
-    except FloatingPointError:
-        print("Floating point Error")
-        return False
-
